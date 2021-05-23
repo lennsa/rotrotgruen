d80f55ca42d450f79e5ec396e298d4607bd7c61b
lennsa <lennsa999@gmx.de>
2021-05-23 23:20:29 +0200

    add nav

---
+++ src/components/Nav.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="nav">
+    <ul>
+      <router-link v-for="(page, index) in pages" :key="index" tag="li" :to="{name: page.path_name}" exact>
+        <a>{{ page.name }}</a>
+      </router-link>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+  data () {
+    return {
+      pages: [
+        {name: 'Offener Brief', path_name: 'Letter'},
+        {name: 'Unterzeichner*innen', path_name: 'Signatories'},
+        {name: 'Presse', path_name: 'Press'},
+        {name: 'Kontakt', path_name: 'Contact'}
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.nav {
+  float: right;
+  font-family: 'Jost', Helvetica, Arial, sans-serif;
+  text-transform: uppercase;
+  color : var(--gray);
+  font-size: 1.5em;
+}
+
+ul {
+  display: flex;
+}
+
+li {
+  padding: 0.1em 0.5em;
+  list-style-type: none;
+}
+
+li + li {
+  border-left: 3px solid var(--gray);
+}
+
+li.router-link-active {
+  color : var(--black);
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+</style>
