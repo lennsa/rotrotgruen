057c03102fe3efda929f21df7d5730b9548a0464
lennsa <lennsa999@gmx.de>
2021-05-23 23:20:37 +0200

    evolve pages

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,52 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <Nav></Nav>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Nav from './components/Nav'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Nav
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  box-sizing: border-box;
 }
+
+:root {
+  --green: rgb(27, 115, 64);
+  --gray: #A6A6A6;
+  --background: rgb(239, 239, 239);
+  --black: black;
+  --white: white;
+}
+
+body {
+  margin: 0;
+}
+
+#app {
+  min-height: 100vh;
+  background-color: var(--background);
+  font-family: 'Barlow', Helvetica, Arial, sans-serif;
+}
+
+h1, h2 {
+  color: var(--green);
+  font-size: 2.5em;
+  font-family: 'Jost', Helvetica, Arial, sans-serif;
+}
+
+.container {
+  padding: 5em 1em 1em;
+}
+
 </style>
