2304d43e5723e162b5ca2c3c10929329bf9ba188
lennsa <lennsa999@gmx.de>
2021-06-30 21:43:09 +0200

    add contributors

--- src/views/Impressum.vue
+++ src/views/Impressum.vue
@@ -14,6 +14,19 @@
       Auf mögliche Änderungen des Inhalts haben wir jedoch keinen Einfluss.<br>
       Für die Inhalte auf verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seite verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar.
     </p>
+    <h2 class="container-item">Webentwicklung</h2>
+    <p class="container-item">
+      <b>Design:</b><br>
+      Jonas Dau
+    </p>
+    <p class="container-item">
+      <b>Umsetzung:</b><br>
+      Lennart Suwe
+    </p>
+    <p class="container-item">
+      <b>Unterschriften Backend:</b><br>
+      Jonathan Fritz
+    </p>
   </div>
 </template>
 
@@ -29,6 +42,11 @@ export default {
 <style scoped>
 h2 {
   font-size: 2rem;
+}
+
+p {
+  display: inline-block;
+  margin-block-start: 0;
 }
 
 </style>
